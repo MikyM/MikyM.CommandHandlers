9131dfaee8b4fea5728316bfa937e1b910fe81a3
Krzysztof Kupisz <kupisz.krzysztof@gmail.com>
2022-05-10 18:31:03 +0200

    init

---
+++ MikyM.CommandHandlers/Helpers/CommandRegistrationConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using Microsoft.Extensions.Options;
+using MikyM.Autofac.Extensions;
+
+namespace MikyM.CommandHandlers.Helpers;
+
+/// <summary>
+/// Command handler options
+/// </summary>
+public sealed class CommandHandlerConfiguration : IOptions<CommandHandlerConfiguration>
+{
+    internal CommandHandlerConfiguration(ContainerBuilder builder)
+    {
+        Builder = builder;
+    }
+
+    internal ContainerBuilder Builder { get; set; }
+
+    /// <summary>
+    /// Gets or sets the default lifetime for base generic data services
+    /// </summary>
+    public Lifetime DefaultLifetime { get; set; } = Lifetime.InstancePerLifetimeScope;
+
+    /// <inheritdoc />
+    public CommandHandlerConfiguration Value => this;
+}
